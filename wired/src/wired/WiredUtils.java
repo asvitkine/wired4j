78632427b140086c8d2357c3bcbc5a9abe580896
Alexei.Svitkine <Alexei.Svitkine@904bf21b-bc17-b740-8c95-663677a0c866>
2010-09-13 05:11:31 +0000

    change implementation of bytesToBase64() as previous one was not compatible with project license

--- wired/src/wired/WiredUtils.java
+++ wired/src/wired/WiredUtils.java
@@ -60,45 +60,7 @@ public class WiredUtils {
 		return buf.toString();
 	}
 
-	private static final char[] BASE64_CHARS = {
-		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-		'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-		'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'
-	};
 	public static String bytesToBase64(byte[] bytes) {
-		int numFullGroups = bytes.length / 3;
-		int numBytesInPartialGroup = bytes.length - 3 * numFullGroups;
-		int resultLen = 4*((bytes.length + 2)/3);
-		StringBuilder sb = new StringBuilder(resultLen);
-
-		// Translate all full groups from byte array elements to Base64
-		int inCursor = 0;
-		for (int i = 0; i < numFullGroups; i++) {
-			int byte0 = bytes[inCursor++] & 0xff;
-			int byte1 = bytes[inCursor++] & 0xff;
-			int byte2 = bytes[inCursor++] & 0xff;
-			sb.append(BASE64_CHARS[byte0 >> 2]);
-			sb.append(BASE64_CHARS[(byte0 << 4)&0x3f | (byte1 >> 4)]);
-			sb.append(BASE64_CHARS[(byte1 << 2)&0x3f | (byte2 >> 6)]);
-			sb.append(BASE64_CHARS[byte2 & 0x3f]);
-		}
-
-		// Translate partial group if present
-		if (numBytesInPartialGroup != 0) {
-			int byte0 = bytes[inCursor++] & 0xff;
-			sb.append(BASE64_CHARS[byte0 >> 2]);
-			if (numBytesInPartialGroup == 1) {
-				sb.append(BASE64_CHARS[(byte0 << 4) & 0x3f]);
-				sb.append("==");
-			} else {
-				int byte1 = bytes[inCursor++] & 0xff;
-				sb.append(BASE64_CHARS[(byte0 << 4)&0x3f | (byte1 >> 4)]);
-				sb.append(BASE64_CHARS[(byte1 << 2)&0x3f]);
-				sb.append('=');
-			}
-		}
-		return sb.toString();
+		return new String(Base64Coder.encode(bytes));
 	}
 }
